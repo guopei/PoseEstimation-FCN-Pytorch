0c253a2bffec140a78f4c82ffd892524b9b4fc07
pei guo <amazingguo@gmail.com>
2018-09-21 15:27:01 -0600

    tweak epochs

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=5 python main.py /mv_users/peiguo/dataset/cub-fewshot/full/ --pretrain --epochs 100 --lr 0.2 --print-freq 100 -b 16 --lr_decay 30 #--visualize 
+CUDA_VISIBLE_DEVICES=5 python main.py /mv_users/peiguo/dataset/cub-fewshot/full/ --pretrain --epochs 100 --lr 0.2 --print-freq 100 -b 16 --lr_decay 80 #--visualize 
